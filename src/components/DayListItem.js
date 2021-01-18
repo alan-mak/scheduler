47132dacc863dddc6118f70d6c91e420e2ba0963
Alan Mak <alanmak95@gmail.com>
2021-01-18 18:41:56 -0500

    Tested the DayListItem with different amount of spots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -5,15 +5,29 @@ import "components/DayListItem.scss";
 const classNames = require('classnames');
 
 export default function DayListItem(props) {
+  const {name, spots} = props;
+  let formatSpots = (spots) => {
+    let text = ""
+    if (spots > 1) {
+      text += `${spots} spots`;
+    } else if (spots > 0) {
+      text += `${spots} spot`;
+    } else {
+      text += `no spots`;
+    }
+    text +=  ` remaining`;
+    return text;
+  }
+  
   let itemClass =  classNames(
     "day-list__item",{
     "day-list__item--selected": props.selected,
-    "day-list__item--full": props.spots === 0
+    "day-list__item--full": spots === 0
     })
   return (
-    <li className={itemClass} onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots}</h3>
+    <li className={itemClass} onClick={() => props.setDay(name)}>
+      <h2 className="text--regular">{name}</h2> 
+      <h3 className="text--light">{formatSpots(spots)}</h3>
     </li>
   );
 }
