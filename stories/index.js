e9f10ed7867f31ef7c6598e8bb052178badfdbc9
Alan Mak <alanmak95@gmail.com>
2021-01-19 16:44:36 -0500

    Refactored Interviewer component

--- stories/index.js
+++ stories/index.js
@@ -97,10 +97,9 @@ storiesOf("InterviewerListItem", module)
       id={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
-      setInterviewer={action("setInterviewer")}
+      setInterviewer={event => action("setInterviewer")(interviewer.id)}
     />
   ));
-
   const interviewers = [
     { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
     { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
