b0cf68db71722f19697e63fcc494519d4f9db0f1
Alan Mak <alanmak95@gmail.com>
2021-01-18 21:08:48 -0500

    Select and set day on scheduler

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,11 +1,7 @@
 import React from "react";
 import DayListItem from './DayListItem'
 
-// const classNames = require('classnames');
-
 export default function DayList(props) {
-  console.log(props)
-  // let { days, day, setDay } = props;
   let parsedDayList = props.days.map(day => <DayListItem 
     key={day.id}
     name={day.name} 
