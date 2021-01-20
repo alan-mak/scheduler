7cea2e19b7037f8a3e9ec83d84b2c4503c14ba0d
Alan Mak <alanmak95@gmail.com>
2021-01-19 22:43:28 -0500

    Imported Header, show and empty to Appointment's index.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,19 @@
 import React from "react";
 
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
+
 import "components/Appointment/styles.scss"
+import { action } from "@storybook/addon-actions";
 
 export default function Appointment(props) {
+  const interview = props.interview
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time}/>
+        {props.interview ? <Show student={interview.student} interviewer={interview.interviewer}/> : <Empty />}
+    </article>
   )
 }
