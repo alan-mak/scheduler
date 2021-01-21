88642273cbc0e1925f5acf28639027613a8a83d0
Alan Mak <alanmak95@gmail.com>
2021-01-21 16:50:56 -0500

    Added comments

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,8 +4,10 @@ function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]);
   const transition = (mode, replace = false) => {
     if (replace) {
+      // If replace we put the new mode then replace the last used mode
       setHistory(history => [mode, ...history.slice(1)])
     } else {
+      // Putting the new mode at the beginning of the array
       setHistory(history => [mode, ...history])
     }
   }
