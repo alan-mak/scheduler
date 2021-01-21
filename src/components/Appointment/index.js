2f87a16bcbf3eb044f35103f055c2e4f5b06ac18
Alan Mak <alanmak95@gmail.com>
2021-01-21 17:56:52 -0500

    Added views when creating and emptying the form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,15 +3,37 @@ import React from "react";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form"
+import useVisualMode from "../../hooks/useVisualMode"
 
 import "components/Appointment/styles.scss"
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
   const interview = props.interview
+  const { mode, transition, back } = useVisualMode(
+    interview ? SHOW : EMPTY
+  )
+
   return (
     <article className="appointment">
-      <Header time={props.time}/>
-        {props.interview ? <Show student={interview.student} interviewer={interview.interviewer}/> : <Empty />}
+      <Header time={props.time} />
+      {mode === EMPTY &&
+        <Empty
+          onAdd={() => transition(CREATE)}
+        />}
+      {mode === SHOW && (
+        <Show
+          student={interview.student}
+          interviewer={interview.interviewer}
+        />)}
+      {mode === CREATE && (
+        <Form 
+          onCancel={() => transition(EMPTY)}
+        />
+      )}
     </article>
   )
 }
