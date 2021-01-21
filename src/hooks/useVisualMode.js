87165526f9c28eb8be7d2c450d09c708eaaed9e8
Alan Mak <alanmak95@gmail.com>
2021-01-21 15:25:59 -0500

    Added back function to go back to previous

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,9 +1,21 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial)
-  const transition = (mode) => setMode(mode)
-  return { mode, transition }
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+  const transition = (mode) => {
+    console.log("LINE 7", mode)
+    history.push(mode)
+    console.log("HISTORY 9", history)
+    setMode(mode);
+  }
+  const back = () => {
+    history.pop();
+    let last = history.length - 1;
+    setMode(history[last])
+    console.log("HISTORY 16", history[last])
+  };
+  return { mode, transition, back }
 };
 
 export default useVisualMode;
