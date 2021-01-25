922c0ea5d5e489713b76c843e5b2a33f0c8ae570
Alan Mak <alanmak95@gmail.com>
2021-01-25 14:28:18 -0500

    Added PropTypes

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,9 +1,10 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import InterviewerListItem from './InterviewerListItem'
 import "components/InterviewerList.scss";
 
-export default function InterviewerList(props) {
+function InterviewerList(props) {
   let parsedInterviewerList = props.interviewers.map(interviewer => <InterviewerListItem
     key={interviewer.id}
     name={interviewer.name}
@@ -22,3 +23,9 @@ export default function InterviewerList(props) {
     </section>
   )
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
+export default InterviewerListItem;
