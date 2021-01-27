bafd3376b62685f78ce62997c5904fb7ffd35e89
Alan Mak <alanmak95@gmail.com>
2021-01-27 15:29:18 -0500

    Testing for booking an interview added

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,24 +1,32 @@
-describe("it should book an interview", () => {
-  it("should book an interview", () => {
-    // Visits the root of our web server
-    cy.visit("http://localhost:8001/api/debug/reset")
-    // Clicks on the "Add" button in the second appointment
-    // Enters their name
-    // Chooses an interviewer
-    // Clicks the save button
-    // Sees the booked appointment
+describe("Appointments", () => {
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset");
 
+    // Visits the root of our web server
+    cy.visit("/");
+
+    cy.contains("Monday");
+  });
+
+  it("should book an interview", () => {
+    // Clicks on the "Add" button in the second appointment
+    cy.get("[alt=Add]").first().click();
+    // Enters their name
+    cy.get('[data-testid=student-name-input]').type("Lydia Miller-Jones");
+    // Chooses an interviewer
+    cy.get("[alt='Sylvia Palmer']").click();
+    // Clicks the save button
+    cy.get(".button--confirm").click();
+    // Sees the booked appointment
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
   })
   it("should cancel an interview", () => {
-    // Visits the root of our web server
-    cy.visit("http://localhost:8001/api/debug/reset")
     // Clicks the delete button for the existing appointment
     // Clicks the confirm button
     // Sees that the appointment slot is empty
   })
   it("should edit an interview", () => {
-    // Visits the root of our web server
-    cy.visit("http://localhost:8001/api/debug/reset")
     // Clicks the edit button for the existing appointment
     // Changes the name and interviewer
     // Clicks the save button
